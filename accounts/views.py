4746770b2aa47519c14ba3146b1d3d1bcc38c42c
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-25 01:56:26 +0300

    Created base.html, form for worker creation and common templates for them

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from accounts.forms import WorkerCreationForm
+
+
+Worker = get_user_model()
+
+
+class SignUpView(generic.CreateView):
+    model = Worker
+    form_class = WorkerCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("accounts:login")
+
+
+class ProfileView(LoginRequiredMixin, generic.TemplateView):
+    template_name = "accounts/profile.html"
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from manager.models import Task
 
-# Create your views here.
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "manager/task_list.html"
+    context_object_name = "tasks"
