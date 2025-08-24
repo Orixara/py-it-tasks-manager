57c607f407ed70b04333866e1a08e20b411d0748
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-24 23:31:54 +0300

    Registered Position and Worker to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import Worker, Position
 
-# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name", )
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "position")
+    list_filter = ("position", "is_staff", "is_active")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("position",)}),
+    )
