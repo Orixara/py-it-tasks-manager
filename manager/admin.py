d712f7a899919be9f67f015faa035c6106b0e76a
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-08-24 23:35:37 +0300

    Registered TaskType and Task to admin panel

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from manager.models import TaskType, Task
 
-# Register your models here.
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name", )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "task_type",
+        "priority",
+        "deadline",
+        "is_completed",
+        "created_by"
+    )
+    list_filter = (
+        "task_type",
+        "priority",
+        "is_completed",
+        "created_at"
+    )
+    search_fields = ("name", "description")
+    filter_horizontal = ("assignees",)
+
